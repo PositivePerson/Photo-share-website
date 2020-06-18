513913f618099165eeedd029db22ca5f4b767d12
Bartek <WonderfulLifeVision@gmail.com>
2020-06-19 00:16:46 +0200

    profile-page mobile version

--- src/app/user-images/user-images.component.ts
+++ src/app/user-images/user-images.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-user-images',
   templateUrl: './user-images.component.html',
@@ -10,6 +12,15 @@ export class UserImagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    $(function () {
+
+      $(".heart").on("click", function () {
+        $(this).toggleClass("heart-blast");
+      });
+    });
   }
 
 }
